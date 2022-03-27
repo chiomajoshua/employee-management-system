b897d104f50b821f2b887e12f6b671278eab3937
Joshua Chioma <chiomajoshua@gmail.com>
2022-03-27 12:26:03 +0100

    Core Extensions

---
+++ src/app/api/EMS.Core/Extensions/CoreExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace EMS.Core
+{
+    public static class Extensions
+    {
+        public static IServiceCollection RegisterGenericRepository(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddGenericRepository<RestaurantContext>();
+            return serviceCollection;
+        }
+    }
+}
